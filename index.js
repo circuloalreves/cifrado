c93de15c061edbbe8a82e2c87694dd6262b89115
circuloalreves <99098099+circuloalreves@users.noreply.github.com>
2022-07-11 22:02:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function cifrar(text, clave) {
     return text.split('').map((t,i)=>(t+clave)).join('')
 }
 
-let r= cifrar('Un dia soleado', 'asd66')
+let r = cifrar('Un dia soleado', 'asd66')
 console.log(r)
 
 function descrifrar(text, clave) {
     return text.split('').join('')
 }
 
-let s=descrifrar(r,'asd66')
-console.log(r)
+let s = descrifrar(r,'asd66')
+console.log(s)
