3f343db155242ed84b899db48d35a77945d41273
circuloalreves <99098099+circuloalreves@users.noreply.github.com>
2022-07-11 21:56:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function cifrar(text, clave) {
+    return text.split('').map((t,i)=>(t+clave)).join('')
+}
+
+let r= cifrar('Un dia soleado', 'asd66')
+console.log(r)
+
+function descrifrar(text, clave) {
+    return text.split('').join('')
+}
+
+let s=descrifrar('Un dia soleado', 'asd66')
+console.log(r)
